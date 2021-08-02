3e6e41837c7c95bfa98fecd83cf9b3e7cab0763c
Martinovdima <martinovdima@rambler.ru>
2021-08-03 00:29:14 +0600

    finish

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -146,11 +146,11 @@ EMAIL_HOST_PASSWORD = 'geekshop'
 EMAIL_USE_SSL = False
 
 #вариант python -m smtpd -n -c DebuggingServer localhost:25
-# EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
 
 #вариант логирования сообщений почты в виде файлов вместо отправки
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = 'tmp/email-messages/'
+#EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+#EMAIL_FILE_PATH = 'tmp/email-messages/'
 
 #Реальная настройка для Яндекс.почты
 #EMAIL_HOST = 'smtp.yandex.net'
