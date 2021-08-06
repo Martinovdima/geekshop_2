042e9e4195f7a80b0359a1b3fd33e949d157f3b4
Martinovdima <martinovdima@rambler.ru>
2021-08-06 11:12:58 +0600

    added Order create

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 app_name = "ordersapp"
 
 urlpatterns = [
-    path('', views.OrderList.as_view(), name='orders_list'),
-    path('read/<pk>/', views.OrderList.as_view(), name='orders_read'),
-    path('update/<pk>/', views.OrderList.as_view(), name='orders_update'),
-    path('delete/<pk>/', views.OrderList.as_view(), name='orders_delete'),
+    path('', views.OrderList.as_view(), name='order_list'),
+    path('create/', views.OrderItemCreate.as_view(), name='order_create'),
+    path('read/<pk>/', views.OrderList.as_view(), name='order_read'),
+    path('update/<pk>/', views.OrderList.as_view(), name='order_update'),
+    path('delete/<pk>/', views.OrderList.as_view(), name='order_delete'),
 ]
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -15,3 +15,31 @@ class OrderList(ListView):
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
+
+class OrderItemCreate(CreateView):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('ordersapp:order_list')
+    
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.method == 'POST':
+            formset = OrderFormset(self.request.POST)
+        else:
+            formset = OrderFormset()
+
+        data['orderitems'] = formset
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        form.instance.user = self.request.user
+        self.object = form.save()
+        if orderitems.is_valid():
+            orderitems.instance = self.object
+            orderitems.save()
+        return super().form_valid(form)
