78e0a39e532d7ed987476592b4ff597ec72f2bab
Martinovdima <martinovdima@rambler.ru>
2021-08-09 17:01:27 +0600

    added price

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -38,6 +38,7 @@ class OrderItemCreate(CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
+                    form.initial['price'] = basket_items[num].product.price
 
             else:
                 formset = OrderFormset()
@@ -73,8 +74,10 @@ class OrderItemUpdate(UpdateView):
             formset = OrderFormset(self.request.POST, instance=self.object)
         else:
             formset = OrderFormset(instance=self.object)
-
-        data['orderitems'] = formset
+            for form in formset.forms:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
+            data['orderitems'] = formset
         return data
 
     def form_valid(self, form):
