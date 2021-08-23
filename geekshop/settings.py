e04dbb29ddf085b6107d8e845bec54e191e16258
Martinovdima <martinovdima@rambler.ru>
2021-08-23 08:42:00 +0600

    STATIC_ROOT

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -83,7 +83,7 @@ if DEBUG:
         'debug_toolbar.panels.profiling.ProfilingPanel',
         'template_profiler_panel.panels.template.TemplateProfilerPanel',
     ]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ROOT_URLCONF = 'geekshop.urls'
 
@@ -167,9 +167,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+#STATICFILES_DIRS = (
+    #os.path.join(BASE_DIR, "static"),
+#)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
