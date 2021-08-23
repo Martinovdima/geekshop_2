95f104ad2890431d5035abdee4d198587944b628
Martinovdima <martinovdima@rambler.ru>
2021-08-23 15:02:14 +0600

    added selected_related in get_hot_product() in views mainapp

--- mainapp/views.py
+++ mainapp/views.py
@@ -18,7 +18,7 @@ def load_from_json(file_name):
 
         
 def get_hot_product():
-    products = Product.objects.filter(is_active=True, category__is_active=True)
+    products = Product.objects.filter(is_active=True, category__is_active=True).select_related()
     
     return random.sample(list(products), 1)[0]
     
