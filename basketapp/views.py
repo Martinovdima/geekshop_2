241a0a80552f3fe175c9d99cb203af8985e7f9e2
Martinovdima <martinovdima@rambler.ru>
2021-08-24 22:51:44 +0600

    added select_relate()

--- basketapp/views.py
+++ basketapp/views.py
@@ -11,8 +11,8 @@ from django.http import JsonResponse
 @login_required
 def basket(request):
     title = 'корзина'
-    
-    basket_items = Basket.objects.filter(user=request.user).order_by('product__category').select_related()
+    user = request.select_related()
+    basket_items = Basket.objects.filter(user=user).order_by('product__category').select_related()
     
     content = {
         'title': title,
