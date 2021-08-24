7a6f950bd80424d4dcc26b4a3533bcd03c7a63a2
Martinovdima <martinovdima@rambler.ru>
2021-08-25 02:09:38 +0600

    added select_related()

--- mainapp/views.py
+++ mainapp/views.py
@@ -52,10 +52,10 @@ def products(request, pk=None, page=1):
                 'pk': 0,
                 'name': 'все'
             }
-            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price').select_related()
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price').select_related()
         
         paginator = Paginator(products, 2)
         try:
